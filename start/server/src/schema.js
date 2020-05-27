ee5c6e84fa26e13a0c3d738d8f74fc97f6fd7b4c
John <jjh5166@gmail.com>
2020-05-26 21:39:14 -0400

    schema notes

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,12 +1,17 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
+  # The Launch object type has a collection of fields, and each field has a type of its own.
+  # A field's type can be either an object type or a scalar type. A scalar type is a primitive 
+  # (like ID, String, Boolean, or Int) that resolves to a single value. In addition to GraphQL's 
+  # built-in scalar types, you can define custom scalar types.
   type Launch {
     id: ID!
     site: String
     mission: Mission
     rocket: Rocket
     isBooked: Boolean!
+    # An exclamation point (!) after a declared field's type means "this field's value can never be null."
   }
   type Rocket {
     id: ID!
@@ -18,6 +23,8 @@ const typeDefs = gql`
     id: ID!
     email: String!
     trips: [Launch]!
+    # If a declared field's type is in [Square Brackets], it's an array of the specified type.
+    # If an array has an exclamation point after it, the array cannot be null, but it can be empty.
   }
 
   type Mission {
@@ -29,21 +36,29 @@ const typeDefs = gql`
     SMALL
     LARGE
   }
+  # A schema needs to define queries that clients can execute against the data graph.
+  # You define your data graph's supported queries as fields of a special type called the Query type.
   type Query {
     launches: [Launch]!
     launch(id: ID!): Launch
     me: User
   }
+  # To enable clients to modify data, our schema needs to define some mutations.
+  # The Mutation type is a special type that's similar in structure to the Query type. 
   type Mutation {
     bookTrips(launchIds: [ID]!): TripUpdateResponse!
     cancelTrip(launchId: ID!): TripUpdateResponse!
     login(email: String): String # login token
   }
+  # A mutation's return type is entirely up to you, but we recommend defining special object 
+  # types specifically for mutation responses.
   type TripUpdateResponse {
     success: Boolean!
     message: String
     launches: [Launch]
   }
+  # It's good practice for a mutation to return whatever objects it modifies so the requesting 
+  # client can update its cache and UI without needing to make a followup query.
 `;
 
 module.exports = typeDefs;
